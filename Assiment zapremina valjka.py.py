c61fe1292ec4b6db4b14f66663500c75897de9d2
vukdakic <153130890+vukdakic@users.noreply.github.com>
2024-01-07 18:11:07 +0100

    Add files via upload

---
+++ Assiment zapremina valjka.py.py
@@ -0,0 +1,8 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> radius = 10
+>>> height = 50
+>>> pi_value = 3.14
+>>> volume = radius**2 * pi_value * height
+>>> print(f"The volume of cylinder is: {volume}")
+The volume of cylinder is: 15700.0
